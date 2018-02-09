73af487f8a2aea8e182100de8b913821029b04ee
chrishshare <chrishshare@163.com>
2018-02-09 20:29:42 +0800

    增加accesstoken

--- wechart/models.py
+++ wechart/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class WXDevInfo(models.Model):
     AppId = models.CharField(max_length=20, verbose_name='开发者ID(AppID)')
     AppSecret = models.CharField(max_length=50, verbose_name='开发者密码(AppSecret)')
+    accesstoken = models.CharField(max_length=256, verbose_name='ACCESSTOKEN')
 
     class Meta:
         verbose_name = '微信公众号基本信息'
