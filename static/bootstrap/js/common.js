e6ecfd63d06ea960a8b735cdd37de8451ec406b0
chrishshare <chrishshare@163.com>
2018-03-07 20:48:32 +0800

    优化url体验，增加loading效果 调整布局，分三栏，预留

--- static/bootstrap/js/common.js
+++ static/bootstrap/js/common.js
@@ -1,7 +1,9 @@
 $(document).ajaxStart(function () {
     $('.loading-tfoot').show();
     $('.loading').show();
+    $('.loader').show();
 }).ajaxStop(function () {
     $('.loading').hide();
     $('.loading-tfoot').hide();
+    $('.loader').hide();
 })
